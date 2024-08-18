25ce1f84c3fb8e37267b10ed33cf3cf316233430
sergiuscain <167008320+sergiuscain@users.noreply.github.com>
2024-08-18 13:59:21 +0300

    Добавил Класс вопроса и класс QuestionStorage

---
+++ WinFormsApp1/Queston.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeniusIdiot_WinForms
+{
+    internal class Question
+    {
+        string question;
+        int answer;
+        public Question(string question, int answer)
+        {
+            this.question = question;
+            this.answer = answer;
+        }
+    }
+}
